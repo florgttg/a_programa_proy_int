e204118d52845da3e2099c75fadff790498697b2
florgttg <122732850+florgttg@users.noreply.github.com>
2023-01-15 13:18:24 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,12 @@
+document.getElementById("natacion").addEventListener('click', function () {
+    console.log("El usuario hizo click en natacion");
+    document.getElementById("vermas").innerHTML = "Primer puesto olimpiadas 2004 categoria infantil.En 2013 fue elegida «Atleta del Año» por la FINA.4​ Fue nombrada «Atleta del Año» por el Comité Olímpico Estadounidense en tres ocasiones: 2013, 2016 y 2017.";
+})
+document.getElementById("atletismo").addEventListener('click', function () {
+    console.log("El usuario hizo click en atletismo");
+    document.getElementById("vermas2").innerHTML = "Practico atletismo desde los 12 años."
+})
+document.getElementById("jpa").addEventListener('click', function () {
+    console.log("El usuario hizo click en juntos por un amigo");
+    document.getElementById("vermas3").innerHTML = "Hace 4 años ayudo a que perros y gatos callejeros encuentren su hogar.Facebook: Juntosx1AmigoAC"
+})
